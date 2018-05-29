a3ec34d9da0b129262c2c50fd5ac750c20dcc2f8
boking <zhb_wang1@163.com>
2018-05-29 10:42:59 +0800

    111

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <ul class="footer">
+      <li class="act">首页</li>
+      <li>首页1</li>
+      <li>首页2</li>      
+    </ul>
+    <router-view/>
+    <div class="spare"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+@r:37.8rem;
+*{
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  // margin-top: 60/@r;
+}
+.footer{
+  font-size: 16/@r;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  height: 50/@r;
+  line-height: 50/@r;
+  background: #fff;
+  position: fixed;
+    bottom: 0;
+    width: 100%;
+  li{
+    width: 33.3%;
+    float: left;
+    &.act{
+      background: #ccc;
+
+    }
+  }
+}
+.spare{
+  height: 50/@r;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <div class='box' v-for="k in 10" :key="k">
+      {{k}}
+    </div>
+  
+
+  </div>
+</template>
+<script scoped>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped   lang="less">
+@r:37.8rem;
+*{
+  margin: 0;
+  padding: 0;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.box{
+  width: 170/@r;
+  height: 170/@r;
+  background: #f00;
+  font-size: 16/@r;
+  float: left;
+  margin: 15/@r 5/@r 0 10/@r;
+}
+</style>
